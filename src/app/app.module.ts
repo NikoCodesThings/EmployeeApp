e28703adb43bbdfcc55099516ff128505e246733
Nick <nespinosa976@gmail.com>
2018-04-13 12:10:19 -0700

    Add To Repo
    Adding Files to repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EmploginComponent } from './emplogin/emplogin.component';
+import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { EditemployeeComponent } from './editemployee/editemployee.component';
+import { DeleteemployeeComponent } from './deleteemployee/deleteemployee.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
 
+const appRoutes: Routes= [
+  {path: '', component: LoginComponent},
+  {path 'home', component: MainpageComponent},
+  {path:'findEmployee', component: ViewemployeeComponent},
+  {path: 'editEmployee', component: EditemployeeComponent},
+  {path: 'addEmployee', component: AddemployeeComponent},
+  {path: 'removeEmployee', component: DeleteemployeeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmploginComponent,
+    LoginComponent,
+    MainpageComponent,
+    MainmenuComponent,
+    AddemployeeComponent,
+    EditemployeeComponent,
+    DeleteemployeeComponent,
+    ViewemployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+          appRoutes,
+          { enableTracing: true } // <-- debugging purposes only
+        )
+        // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
