4e59ce299c58e95cbcb6ff9e253309f627a7ba6c
Nick <nespinosa976@gmail.com>
2018-04-13 16:01:43 -0400

    Auto stash before revert of "Update Site Flow"

---
+++ src/app/viewemployees/viewemployees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewemployees',
+  templateUrl: './viewemployees.component.html',
+  styleUrls: ['./viewemployees.component.css']
+})
+export class ViewemployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
